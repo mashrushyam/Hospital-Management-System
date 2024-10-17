87dbedef2370098f722c16a443e48fdc9d2b2605
Shyam-Mashru <mashrushyam030303@gmail.com>
2024-10-18 00:55:50 +0530

    Some Changes Project on Hold

--- Hospital Management System/obj/Debug/net8.0/Hospital Management System.AssemblyInfo.cs
+++ Hospital Management System/obj/Debug/net8.0/Hospital Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9efdef6954dd8222eb2099fa7692058790e0781a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1540d39c4241e675da031952d161e3e7c93ccc17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
